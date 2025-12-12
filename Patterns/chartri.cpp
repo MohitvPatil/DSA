e9a65f15fd2cd21d1df5ecd6faee228f5569ddd6
Mohit Patil <mohitvpatil2003@gmail.com>
2025-12-12 18:09:18 +0530

    1

--- Patterns/chartri.cpp
+++ Patterns/chartri.cpp
@@ -1,14 +1,24 @@
 using namespace std;
 #include <iostream>
 
+void print(int n){
+    int num = 1;    
+    for(int i = 1 ; i <= n ;  i++){
+        for(int j = 1 ; j <= i  ; j++){
+            cout << num ;
+            num = num + 1;
+        }
+        cout << endl;
+    }
+}
+
+
 // void print(int n){
-//     int num = 1;
-//     for(int i = 1 ; i <= n ;  i++){
-//         for(int j = 1 ; j <= i  ; j++){
-//             cout << num ;
-//             num = num + 1;
+//     for(int i=0; i<n ; i++){
+//         for(char ch = 'A' ; ch <= 'A' + (n-i-1) ; ch++ ){
+//             cout << ch << " ";
 //         }
-//         cout << endl;
+//        cout << endl; 
 //     }
 // }
 
@@ -25,23 +35,14 @@ using namespace std;
 
 // void print(int n){
 //     for(int i=0; i<n ; i++){
-//         for(char ch = 'A' ; ch <= 'A' + (n-i-1) ; ch++ ){
+//         char ch = 'A' + i;
+//         for(int j =0 ; j<=i ; j++ ){
 //             cout << ch << " ";
 //         }
 //        cout << endl; 
 //     }
 // }
 
-
-void print(int n){
-    for(int i=0; i<n ; i++){
-        char ch = 'A' + i;
-        for(int j =0 ; j<=i ; j++ ){
-            cout << ch << " ";
-        }
-       cout << endl; 
-    }
-}
 
 int main(){ 
     int n;
